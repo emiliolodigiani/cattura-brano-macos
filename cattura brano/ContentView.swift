37a7253bb35cde63dae32b08e868313145ed2485
Emilio Lodigiani <emilio.lodigiani@tunca.it>
2026-09-01 11:29:53 +0200

    Initial Commit

---
+++ cattura brano/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cattura brano
+//
+//  Created by Emilio Alfredo Lodigiani on 01/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cattura brano/cattura_branoApp.swift
@@ -0,0 +1,17 @@
+//
+//  cattura_branoApp.swift
+//  cattura brano
+//
+//  Created by Emilio Alfredo Lodigiani on 01/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct cattura_branoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
